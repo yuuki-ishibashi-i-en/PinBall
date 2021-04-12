70606f289b8a4409d994e8ac84e7dc8ec30d798d
yuuki-ishibashi-i-en <y-ishibashi@i-enter.co.jp>
2021-04-12 16:32:43 +0900

    Initial commit

---
+++ Assets/Script/StarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour
+{
+    private float rotSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.Rotate(0, Random.Range(0, 360), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, this.rotSpeed, 0);
+    }
+}
