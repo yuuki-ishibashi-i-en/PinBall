ce3a6d2e7737044d93531c843a18acc9dbe7904f
yuuki-ishibashi-i-en <y-ishibashi@i-enter.co.jp>
2021-04-13 11:18:50 +0900

    課題終了
    A/Dの入力にも対応

--- Assets/Script/BrightnessRegulator.cs
+++ Assets/Script/BrightnessRegulator.cs
@@ -3,19 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Point_Add
-{
-    public int PointCount = 0;
-
-    public int PointPlus(int i)
-    {
-        this.PointCount+=i;
-
-        return this.PointCount;
-    }
-}
-
-
 public class BrightnessRegulator : MonoBehaviour
 {
     Material myMaterial;
@@ -30,37 +17,30 @@ public class BrightnessRegulator : MonoBehaviour
 
     Color defaultColor = Color.white;
 
-    Point_Add pc = new Point_Add();
 
-    private GameObject PointCountText;
 
     // Use this for initialization
     void Start()
     {
-        this.PointCountText = GameObject.Find("PointCountText");
 
         if (tag == "SmallStarTag")
         {
             this.defaultColor = Color.white;
-            pc.PointPlus(10);
         }
 
         else if (tag == "LargeStarTag")
         {
             this.defaultColor = Color.yellow;
-            pc.PointPlus(150);
         }
 
         else if (tag == "SmallCloudTag")
         {
             this.defaultColor = Color.cyan;
-            pc.PointPlus(30);
         }
 
         else if (tag == "LargeCloudTag")
         {
             this.defaultColor = Color.cyan;
-            pc.PointPlus(500);
         }
 
 
@@ -72,7 +52,6 @@ public class BrightnessRegulator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.PointCountText.GetComponent<Text>().text = "得点" + pc.PointCount;
 
         if (this.degree >= 0)
         {
@@ -83,6 +62,7 @@ public class BrightnessRegulator : MonoBehaviour
 
             this.degree -= this.speed;
         }
+
     }
 
     void OnCollisionEnter(Collision other)
---
+++ Assets/Script/PointControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Point_Add
+{
+    public int PointCount = 0;
+
+    public int PointPlus(int i)
+    {
+        this.PointCount += i;
+
+        return this.PointCount;
+    }
+}
+
+public class PointControl : MonoBehaviour
+{
+
+    private GameObject PointCountText;
+
+    Point_Add pc = new Point_Add();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.PointCountText = GameObject.Find("PointCountText");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.PointCountText.GetComponent<Text>().text = "得点" + pc.PointCount;
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            pc.PointPlus(10);
+        }
+
+        if (other.gameObject.tag == "LargeStarTag")
+        {
+            pc.PointPlus(50);
+        }
+
+        if (other.gameObject.tag == "SmallCloudTag")
+        {
+            pc.PointPlus(40);
+        }
+
+        if (other.gameObject.tag == "LargeCloudTag")
+        {
+            pc.PointPlus(200);
+        }
+
+    }
+}
